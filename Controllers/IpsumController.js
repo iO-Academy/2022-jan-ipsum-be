939535477b47ff8f9e7c716f11a919bdec8e56d8
Marcus Drury <marcusdrury@outlook.com>
2022-04-28 16:21:39 +0100

    fixed validation

--- Controllers/IpsumController.js
+++ Controllers/IpsumController.js
@@ -10,6 +10,11 @@ const getIpsum = async (req, res) => {
         paragraphs = req.query.paragraphs
     }
 
+    if (req.query.paragraphs && !(req.query.paragraphs >= 1 && req.query.paragraphs <= 10)) {
+        let apiResponse = JsonResponse([], false, 'Please pick a number between 1 and 10');
+         return res.json(apiResponse)
+    }
+
     let collection = await DbService('ipsum')
     let data = await getIpsumService(collection)
     let result = multipleParagraphs(paragraphs, data)
